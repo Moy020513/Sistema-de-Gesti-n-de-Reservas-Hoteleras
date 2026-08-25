4cc69eb8956d16f445594fcf4a941b2685c6add8
jdamian1039 <damiangalvan1039@gmail.com>
2026-08-25 12:27:56 -0600

    Entidad Huesped y enums

---
+++ commons/src/main/java/com/damoi/commons/enums/EstadoRegistro.java
@@ -0,0 +1,6 @@
+package com.damoi.commons.enums;
+
+public enum EstadoRegistro {
+    ACTIVO,
+    ELIMINADO
+}
---
+++ commons/src/main/java/com/damoi/commons/enums/EstadoReserva.java
@@ -0,0 +1,8 @@
+package com.damoi.commons.enums;
+
+public enum EstadoReserva {
+    CONFIRMADA,
+    EN_CURSO,
+    FINALIZADA,
+    CANCELADA
+}
