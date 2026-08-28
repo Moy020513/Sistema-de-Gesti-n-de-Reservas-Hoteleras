790b50d39805fa6375eee8febf553676d4d30138
Moy020513 <l20590306@itsanjuan.edu.mx>
2026-08-28 13:38:37 -0600

    Version final, creo c)

--- reservas/src/main/java/com/damoi/reservas/controllers/ReservacionController.java
+++ reservas/src/main/java/com/damoi/reservas/controllers/ReservacionController.java
@@ -28,8 +28,8 @@ public class ReservacionController extends CommonController<ReservacionRequest, 
     }
 
     @PutMapping("{idReservacion}/estado/{idEstadoReservacion}")
-    public ResponseEntity<Void> actualizarEstadoReserva(@PathVariable Long idReservacion, @PathVariable Long idEstadoRegistro){
-        service.actualizarEstadoReservacion(idReservacion, idEstadoRegistro);
+    public ResponseEntity<Void> actualizarEstadoReserva(@PathVariable Long idReservacion, @PathVariable Long idEstadoReservacion){
+        service.actualizarEstadoReservacion(idReservacion, idEstadoReservacion);
         return ResponseEntity.noContent().build();
     }
 
